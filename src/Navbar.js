8f3ed25a74051eadc65859279517012982530957
FanuelNyoni <Fancyee10@gmail.com>
2021-02-23 00:29:40 +0200

    Press 'Enter' to confirm or 'Escape' to cancel

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"; //use Link for 'a' tags to handle all links on browser
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Pimp Clinic Blog</h1>
             <div className="links">
-                <a href = "/">Home</a>
-                <a href = "/create">New Blog</a>
+                <Link to = "/">Home</Link>
+                <Link to = "/create">New Blog</Link>
             </div>
         </nav>
     );
